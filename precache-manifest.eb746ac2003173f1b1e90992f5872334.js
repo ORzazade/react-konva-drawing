9256df745d1217600d1ef01d1dc386c48e71ca86
o.rzazade <o.rzazade@gmail.com>
2019-11-13 12:56:53 +0200

    Updates

---
+++ precache-manifest.eb746ac2003173f1b1e90992f5872334.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c593c1bbcc15d6af81f703b045711ea",
+    "url": "/react-konva-drawing/index.html"
+  },
+  {
+    "revision": "13a38f05ceb0402ed1e5",
+    "url": "/react-konva-drawing/static/css/2.a88e713f.chunk.css"
+  },
+  {
+    "revision": "49841337fff582e9e7d5",
+    "url": "/react-konva-drawing/static/css/main.a3d7dd86.chunk.css"
+  },
+  {
+    "revision": "13a38f05ceb0402ed1e5",
+    "url": "/react-konva-drawing/static/js/2.5c16be13.chunk.js"
+  },
+  {
+    "revision": "49841337fff582e9e7d5",
+    "url": "/react-konva-drawing/static/js/main.166e3267.chunk.js"
+  },
+  {
+    "revision": "acaf81981bd01e99acec",
+    "url": "/react-konva-drawing/static/js/runtime-main.33e3b113.js"
+  },
+  {
+    "revision": "3627ce4badfc137d79553e834c537b10",
+    "url": "/react-konva-drawing/static/media/Erangel_Main.3627ce4b.jpg"
+  },
+  {
+    "revision": "8f33f0cb48500ed5a6d4fa97efd6d659",
+    "url": "/react-konva-drawing/static/media/RedPencil.8f33f0cb.cur"
+  }
+]);
